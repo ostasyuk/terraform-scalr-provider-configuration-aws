02c19317f9aba8138b69e610ec3f64b468df127c
Edhar Mocharnyk <e.mocharnyk@scalr.com>
2022-07-15 13:24:46 +0300

    switched to the production registry

--- providers.tf
+++ providers.tf
@@ -1,9 +1,8 @@
 terraform {
   required_providers {
     scalr = {
-      # TODO: replace to production registry after the release.
-      source = "registry.scalr.dev/scalr/scalr"
-      version= "1.0.0-rc-develop"
+      source = "registry.scalr.io/scalr/scalr"
+      version= "1.0.0-rc32"
     }
   }
 }
